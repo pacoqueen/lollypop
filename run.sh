dc4cb6419e19a7d0289e5672574aaaf7ad824be5
Francisco José Rodríguez Bogado <bogado@qinn.es>
2018-06-14 13:26:58 +0200

    REDUX

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 ninja -C build install
+echo "Killing lollypop-portal"
+kill -9 $(ps uax | grep lollypop-portal | grep python | awk '{print $2}')
 echo "Running lollypop"
 # lollypop -e
-lollypop -d
-# lollypop
+# lollypop -d
+lollypop
